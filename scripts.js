53b587de66201a7077d1fd88f5c12533950eb744
unknown <dibenvishvaa@gmail.com>
2020-08-01 22:02:09 +0800

    check only letter

--- scripts.js
+++ scripts.js
@@ -2,7 +2,12 @@ function testResults(form) {
   var word = form.inputbox.value;
   var vowels = "aeiouAEIOU";
   var matchVowels = word.match(/[aeiou]/gi);
-  if (vowels.indexOf(word[0]) != -1) {
+  var checkIfLetters = !/[^a-z]/i.test(word) ;
+ 
+  if(checkIfLetters == false){
+   alert("Only Letters without whitespace,numbers or signs");
+  }
+  else if (vowels.indexOf(word[0]) != -1) {
     return (document.getElementById("result_here").innerHTML = word);
   } else if (vowels.indexOf(word[0]) == -1 && matchVowels != null) {
     var firstStringChar = word.charAt(0);
@@ -11,6 +16,6 @@ function testResults(form) {
     return (document.getElementById("result_here").innerHTML =
       word + firstStringChar + "ay");
   } else if (matchVowels == null) {
-    return (document.getElementById("result_here").innerHTML =  word + "ay");
+    return (document.getElementById("result_here").innerHTML = word + "ay");
   }
 }
